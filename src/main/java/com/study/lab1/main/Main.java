1dae52ed98e0cbdf9609dbdb3fa64cca68fee6fe
Trubin Tolik <trubintolik@gmail.com>
2016-02-04 01:59:36 +0200

    initial commit

---
+++ src/main/java/com/study/lab1/main/Main.java
@@ -0,0 +1,20 @@
+package com.study.lab1.main;
+
+import com.study.lab1.servlets.AllRequestsServlet;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AllRequestsServlet allRequestsServlet = new AllRequestsServlet();
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(allRequestsServlet), "/*");
+
+        Server server = new Server(8080);
+        server.setHandler(context);
+
+        server.start();
+    }
+}
